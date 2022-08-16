8c75d730414a0e75fd9472ccca190f788a67fcab
duellal <alexandriaduell@gmail.com>
2022-08-15 21:13:16 -0400

    Working on styling the contact form page

--- src/components/contact.js
+++ src/components/contact.js
@@ -2,6 +2,8 @@ import React from 'react';
 
 import { Header, Footer } from './header-footer'
 import StyledButtons from '../styles/buttonStyles'
+import { StyledFormDiv, StyledInputDiv, StyledContactLabels, StyledInputs, StyledH2 } from '../styles/contactFormStyles';
+import { StyledForm } from '../styles/loginPageStyles'
 
 export default function Contact(props) {
    const { changeContact, valuesContact, contactErrors } = props
@@ -16,91 +18,87 @@ export default function Contact(props) {
       <>
          <Header />
 
-         <form id='contact-form' onSubmit={null}>
-            <div className='contact-div'>
-               <h2>
-                  Contact the Team
-               </h2>
-               <p>
-                  If you would like to get in touch with the team, feel free to fill out the form below.
-               </p>
-               <div className='contact-first-name'>
+         <StyledForm onSubmit={null}>
+            <StyledH2>
+               Contact the Team
+            </StyledH2>
+            <p className='contact-p'>
+               If you would like to get in touch with the team, feel free to fill out the form below.
+            </p>
+            <StyledFormDiv>
+               <StyledInputDiv>
+                  <StyledContactLabels>
+                     First Name
+                  </StyledContactLabels>
+                  <StyledInputs
+                     type='text'
+                     name='first_name'
+                     value={valuesContact.first_name}
+                     onChange={onChangeContact}
+                  />
                   <div className='errors'>
                      {contactErrors.first_name}
                   </div>
-                  <label>
-                     First Name
-                     <input
-                        type='text'
-                        id='input-first-name'
-                        name='first_name'
-                        value={valuesContact.first_name}
-                        onChange={onChangeContact}
-                     />
-                  </label>
-               </div>
-               <div className='contact-last-name'>
+               </StyledInputDiv>
+               <StyledInputDiv>
+                  <StyledContactLabels>
+                     Last Name
+                  </StyledContactLabels>
+                  <StyledInputs
+                     type='text'
+                     name='last_name'
+                     value={valuesContact.last_name}
+                     onChange={onChangeContact}
+                  />
                   <div className='errors'>
                      {contactErrors.last_name}
                   </div>
-                  <label>
-                     Last Name
-                     <input
-                        type='text'
-                        id='input-last-name'
-                        name='last_name'
-                        value={valuesContact.last_name}
-                        onChange={onChangeContact}
-                     />
-                  </label>
-               </div>
-               <div className='contact-un'>
-                  <label>
+               </StyledInputDiv>
+               <StyledInputDiv>
+                  <StyledContactLabels>
                      Username
-                     <input
-                        type='text'
-                        id='input-un'
-                        name='username'
-                        value={valuesContact.username}
-                        onChange={onChangeContact}
-                     />
-                  </label>
-               </div>
-               <div className='contact-subject'>
+                  </StyledContactLabels>
+                  <StyledInputs
+                     type='text'
+                     name='username'
+                     value={valuesContact.username}
+                     onChange={onChangeContact}
+                  />
+               </StyledInputDiv>
+               <StyledInputDiv>
+                  <StyledContactLabels>
+                     Subject
+                  </StyledContactLabels>
+                  <StyledInputs
+                     type='text'
+                     name='subject'
+                     value={valuesContact.subject}
+                     onChange={onChangeContact}
+                  />
                   <div className='errors'>
                      {contactErrors.subject}
                   </div>
-                  <label>
-                     Subject
-                     <input
-                        type='text'
-                        id='input-subject'
-                        name='subject'
-                        value={valuesContact.subject}
-                        onChange={onChangeContact}
-                     />
-                  </label>
-               </div>
+               </StyledInputDiv>
                <div className='contact-message'>
+                  <StyledContactLabels>
+                     Message
+                  </StyledContactLabels>
+                  <StyledInputs
+                     type='text'
+                     className='message-input'
+                     name='message'
+                     value={valuesContact.message}
+                     onChange={onChangeContact}
+                  />
                   <div className='errors'>
                      {contactErrors.message}
                   </div>
-                  <label>
-                     Message
-                     <input
-                        type='text'
-                        id='input-message'
-                        name='message'
-                        value={valuesContact.message}
-                        onChange={onChangeContact}
-                     />
-                  </label>
                </div>
-               <div className="button">
+               <div className="contact-button">
                   <StyledButtons>Submit</StyledButtons>
                </div>
-            </div>
-         </form>
+            </StyledFormDiv>
+         </StyledForm>
 
          <Footer />
       </>
--- src/styles/contactFormStyles.js
+++ src/styles/contactFormStyles.js
@@ -1,7 +1,49 @@
 import styled from 'styled-components';
 
-// const StyledContactForm = styled.form`
+//May not need:
+const StyledContactForm = styled.form`
+   display: flex;
+   justify-content: center;
+   padding: 4% 0 2% 0;
+`
 
-// `
+const StyledH2 = styled.h2`
+   font-size: 4rem;
+   font-weight: 600;
+   padding-bottom: 2%;
+   width: 100%;
+`
 
-// export { StyledContactForm }
+const StyledFormDiv = styled.div`
+   display: flex;
+   justify-content: space-around;
+   flex-wrap: wrap;
+   width: 60%;
+   border: 5px solid black;
+   margin: 2% 0 2% 0;
+   padding: 2%;
+`
+
+const StyledInputDiv = styled.div`
+   width: 45%;
+   
+`
+
+const StyledContactLabels = styled.label`
+   width: 100%;
+   font-size: 2rem;
+   padding: 1%;
+   display: flex;
+   justify-content: center;
+   margin: 4% 0 3% 0;
+`
+
+const StyledInputs = styled.input`
+   width: 100%;
+   margin-bottom: 4%;
+   height: 4vh;
+   font-size: 1.8rem;
+   padding: 0.5% 1.5% 0.5% 1.5%;
+`
+
+export { StyledFormDiv, StyledInputDiv, StyledContactLabels, StyledInputs, StyledH2 }
